d3e7ed8022dfc89fd26a05d1e810a59aa3ffc24c
Lennox Herrera <leherreranu@est.utn.ac.cr>
2026-06-30 16:16:06 -0600

    Se agregaron los class de usuarios y materiales bibliograficos al class Devolucion

--- src/devoluciones/Devolucion.java
+++ src/devoluciones/Devolucion.java
@@ -4,10 +4,60 @@
  */
 package devoluciones;
 
+import java.time.LocalDate;
+import usuarios.Usuario;
+import materialesBibliograficos.Materialbibliografico;
+
 /**
  *
- * @author Student
+ * @author Lennox
  */
 public class Devolucion {
+
+    private LocalDate fechaDevolucion;
+    private LocalDate fechaMaximadeDevo;
+    private Usuario ususarios[];
+    private Materialbibliografico material;
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public LocalDate getFechaMaximadeDevo() {
+        return fechaMaximadeDevo;
+    }
+
+    public void setFechaMaximadeDevo(LocalDate fechaMaximadeDevo) {
+        this.fechaMaximadeDevo = fechaMaximadeDevo;
+    }
+
+    public Materialbibliografico getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Materialbibliografico material) {
+        this.material = material;
+    }
+
+    public Usuario[] getUsusarios() {
+        return ususarios;
+    }
+
+    public void setUsusarios(Usuario[] ususarios) {
+        this.ususarios = ususarios;
+    }
+
+    public Devolucion(LocalDate fechaDevolucion, LocalDate fechaMaximadeDevo, Usuario[] ususarios, Materialbibliografico material) {
+        this.fechaDevolucion = fechaDevolucion;
+        this.fechaMaximadeDevo = fechaMaximadeDevo;
+        this.ususarios = ususarios;
+        this.material = material;
+    }
     
+    
+
 }
